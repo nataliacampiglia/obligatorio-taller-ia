25cbcfd91e7ceaf63bd9d9bb2118c6d1857fe47d
naticampiglia <campiglianatalia@gmail.com>
2025-06-24 19:45:41 -0300

    Check rewars improve

--- abstract_agent.py
+++ abstract_agent.py
@@ -139,7 +139,7 @@ class Agent(ABC):
                 os.makedirs(DDQN_BREAKPOINT_DIR, exist_ok=True)
                 torch.save(self.online_net.state_dict(), f"{DDQN_BREAKPOINT_DIR}/GenericDDQNAgent-run-{self.run_name}-steps-{total_steps}-e-{epsilon:.4f}-max_r-{reward}.dat")
             
-        if total_steps % 200000 == 0:
+        if total_steps % 150000 == 0:
             # mostrar recompensa actual y epsilon, y crear un salto de linea
             print(f"=== Recompensa actual: {reward}, Epsilon: {epsilon}, Total steps: {total_steps} ===")
 
